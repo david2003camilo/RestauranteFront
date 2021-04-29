0b82ea903569e2da724416a07f7b87bf540dd606
Mar Marin <marin.franco1604@gmail.com>
2021-04-28 20:02:06 -0500

    se agrego los componentes de home y acerca de nosotros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    FooterComponent,
+    NosotrosComponent
   ],
   imports: [
     BrowserModule,
